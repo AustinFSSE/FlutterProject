ebf99cd934e673d20da523b0d119a338183f4ccd
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2023-12-05 13:28:22 -0500

    Search Bar without Functionality

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -73,7 +73,7 @@ class HomePage extends StatelessWidget {
 
 Container _searchField() {
   return Container(
-    margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+    margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
     decoration: BoxDecoration(boxShadow: [BoxShadow(color: const Color(0xff1D1617).withOpacity(0.11), blurRadius: 40, spreadRadius: 0.0)]),
     child: TextField(
       decoration: InputDecoration(
