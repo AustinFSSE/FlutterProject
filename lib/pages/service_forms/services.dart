a6ec1087b06beb3be061b4a39a4b2cfa61ba2dcd
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2023-12-23 13:08:44 -0500

    New Layout design for the home page

--- lib/pages/service_forms/services.dart
+++ lib/pages/service_forms/services.dart
@@ -4,6 +4,7 @@ import 'package:constructionapp/pages/service_forms/electrical.dart';
 import 'package:constructionapp/pages/service_forms/hvac.dart';
 import 'package:constructionapp/pages/service_forms/plumbing.dart';
 import 'package:constructionapp/pages/service_forms/roofing.dart';
+import 'package:constructionapp/pages/service_forms/window.dart';
 import 'package:flutter/material.dart';
 
 class ServicesOffered extends StatefulWidget {
@@ -20,56 +21,68 @@ class _ServicesOfferedState extends State<ServicesOffered> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              children: [
-                ServiceWidget(
-                  iconsize: 50,
-                  forum: const HVACForum(),
-                  elevation: 4,
-                  size: 50,
-                  iconpath: 'assets/icons/HVAC.svg',
-                ),
-                const Text('HVAC')
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ServiceWidget(
+                    iconsize: 50,
+                    forum: const HVACForum(),
+                    elevation: 4,
+                    size: 50,
+                    iconpath: 'assets/icons/HVAC.svg',
+                  ),
+                  const Text('HVAC')
+                ],
+              ),
             ),
-            Column(
-              children: [
-                ServiceWidget(
-                  iconsize: 50,
-                  forum: const PlumbingForum(),
-                  size: 50,
-                  iconpath: 'assets/icons/plumbing.svg',
-                  elevation: 4,
-                ),
-                const Text('Plumbing')
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ServiceWidget(
+                    iconsize: 50,
+                    forum: const PlumbingForum(),
+                    size: 50,
+                    iconpath: 'assets/icons/plumbing.svg',
+                    elevation: 4,
+                  ),
+                  const Text('Plumbing')
+                ],
+              ),
             ),
-            Column(
-              children: [
-                ServiceWidget(
-                  iconsize: 50,
-                  forum: const RoofingForum(),
-                  size: 50,
-                  iconpath: 'assets/icons/roofing.svg',
-                  elevation: 4,
-                ),
-                const Text('Roofing'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ServiceWidget(
+                    iconsize: 50,
+                    forum: const RoofingForum(),
+                    size: 50,
+                    iconpath: 'assets/icons/roofing.svg',
+                    elevation: 4,
+                  ),
+                  const Text('Roofing'),
+                ],
+              ),
             ),
-            Column(
-              children: [
-                ServiceWidget(
-                  iconsize: 50,
-                  forum: const ElectricalForum(),
-                  size: 50,
-                  iconpath: 'assets/icons/electrical.svg',
-                  elevation: 4,
-                ),
-                const Text('Electrical'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ServiceWidget(
+                    iconsize: 50,
+                    forum: const ElectricalForum(),
+                    size: 50,
+                    iconpath: 'assets/icons/electrical.svg',
+                    elevation: 4,
+                  ),
+                  const Text('Electrical'),
+                ],
+              ),
             ),
           ],
         ),
@@ -87,22 +100,259 @@ class _ServicesOfferedState extends State<ServicesOffered> {
         ),
         Padding(
           padding: const EdgeInsets.only(top: 8.0),
-          child: Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Column(
-                children: [
-                  ServiceWidget(
-                    iconsize: 50,
-                    forum: const DoorServiceForum(),
-                    size: 50,
-                    iconpath: 'assets/icons/door.svg',
-                    elevation: 4,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const DoorServiceForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/door.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Doors',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/window.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Windows',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/furniture.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Furniture',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/shelves.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Shelves',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Custom Designs',
+                style: TextStyle(fontFamily: 'Poppins', color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      ServiceWidget(
+                        iconsize: 50,
+                        forum: const DoorServiceForum(),
+                        size: 50,
+                        iconpath: 'assets/icons/door.svg',
+                        elevation: 4,
+                      ),
+                      const Text(
+                        'Outdoor\nFurniture',
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'Doors',
-                  ),
-                ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/window.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Outdoor\n\t\tSheds',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/furniture.svg',
+                      elevation: 4,
+                    ),
+                    const Center(
+                      child: Text(
+                        'Above Ground\nFirepits',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/shelves.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Small\nPorches',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Remodeling',
+                style: TextStyle(fontFamily: 'Poppins', color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const DoorServiceForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/door.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Bathrooms',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/window.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Kitchens',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/furniture.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Cabinets',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ServiceWidget(
+                      iconsize: 50,
+                      forum: const WindowForum(),
+                      size: 50,
+                      iconpath: 'assets/icons/shelves.svg',
+                      elevation: 4,
+                    ),
+                    const Text(
+                      'Office/Bedrooms',
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
---
+++ lib/pages/service_forms/window.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WindowForum extends StatelessWidget {
+  const WindowForum({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('data'));
+  }
+}
